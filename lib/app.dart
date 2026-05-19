60e651be1678f7c3b94ad37137bdd245e8929db7
Baba Shree Baniya <babashreebaniya@gmail.com>
2026-05-19 21:31:57 +0545

    app.dart

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:calculator/screens/calculator_screen.dart';
+
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Apps for College',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Calculatorscreen(),
+    );
+  }
+}
