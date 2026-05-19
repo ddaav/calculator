0c9342961fd42c9fa0bbae7c149f1612dfc7f4f8
Baba Shree Baniya <babashreebaniya@gmail.com>
2026-05-19 21:29:05 +0545

    error fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:calculator/screens/calculator_screen.dart';
 
-void main() {
-  runApp(const myApp());
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Apps for College',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Calculatorscreen(),
+    );
+  }
 }
